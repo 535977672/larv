9d02ec6886f3b43a5feb0db1bc647d634f5d0f2b
mafei <535977672@qq.com>
2019-07-15 17:26:02 +0800

    '1'

--- app/Http/Controllers/Admin/GoodsController.php
+++ app/Http/Controllers/Admin/GoodsController.php
@@ -49,7 +49,76 @@ class GoodsController extends AdminController
     public function save($id = 0)
     {
         $data = $this->request->all();
+        $goods_name = $data['goods_name'];
+        $type = $data['type'];
+        $url = $data['url'];
+        $brand = $data['brand'];
+        $limit = $data['limit'];
+        $addr = $data['addr'];
+        $cost = $data['cost'];
+        $attr = $data['attr'];//array
+        $is_on_sale = $data['is_on_sale'];//on off
+        $is_hot = $data['is_hot'];
+        $is_new = $data['is_new'];
+        $is_recommend = $data['is_recommend'];
+        $give_integral = $data['give_integral'];
+        $sort = $data['sort'];
+        $original_img = $data['original_img'];
         
+        $cover_thumb = $data['cover_thumb'];
+        $cover_preview = $data['cover_preview'];
+        
+        $price_color_thumb = $data['price_color_thumb'];
+        $price_color_preview = $data['price_color_preview'];
+        $price_spec_name = $data['price_spec_name'];
+        $price_spec_price = $data['price_spec_price'];
+        $price_spec_count = $data['price_spec_count'];
+        $price_spec_real_price = $data['price_spec_real_price'];
+        $price_spec_real_count = $data['price_spec_real_count'];
+        
+        $content = $data['content'];
+        
+        $spec = [];
+        if($price_color_thumb){
+            foreach ($price_color_thumb as $k=>$color){
+                if($color){
+                    $temp = [];
+                    $temp['price_color_thumb'] = $color;
+                    $temp['price_color_preview'] = $price_color_preview[$k];
+                    foreach ($price_spec_name[$k] as $s=>$p){
+                        if($p){
+                            $temp2 = [];
+                            $temp2['price_spec_name'] = $price_spec_name[$k][$s];//name/img
+                            $temp2['price_spec_price'] = $price_spec_price[$k][$s];
+                            $temp2['price_spec_count'] = $price_spec_count[$k][$s];
+                            $temp2['price_spec_real_price'] = $price_spec_real_price[$k][$s];
+                            $temp2['price_spec_real_count'] = $price_spec_real_count[$k][$s];
+                            $temp['spec'] = $temp2;
+                        }
+                    }
+                    $spec[] = $temp;
+                }
+            }
+            
+        }else{
+            foreach ($price_spec_name as $k=>$color){
+                if($color){
+                    $temp = [];
+                    foreach ($price_spec_name[$k] as $s=>$p){
+                        if($p){
+                            $temp2 = [];
+                            $temp2['price_spec_name'] = $price_spec_name[$k][$s];//name/img
+                            $temp2['price_spec_price'] = $price_spec_price[$k][$s];
+                            $temp2['price_spec_count'] = $price_spec_count[$k][$s];
+                            $temp2['price_spec_real_price'] = $price_spec_real_price[$k][$s];
+                            $temp2['price_spec_real_count'] = $price_spec_real_count[$k][$s];
+                            $temp['spec'] = $temp2;
+                        }
+                    }
+                    $spec[] = $temp;
+                }
+            }
+        }
         
         return return_ajax(0, '保存失败', $data);
     }
