7d668d4abcea29d6712e7339bda7d93cd43b4697
mafei <535977672@qq.com>
2019-09-20 15:18:18 +0800

    '1'

--- app/Service/Encrypt.php
+++ app/Service/Encrypt.php
@@ -12,16 +12,7 @@ class Encrypt extends Service{
      * @return 密文
      */
     public static function encrypt($sourceString, $password) {
-        $sourceString = base64_encode($sourceString);
-        $p = str_split($password);
-        $n = count($p);
-        $c = str_split($sourceString);
-        $m = count($c);
-        for($i = 0; $i < $m; $i++){
-            $mima = mb_ord($c[$i]) + mb_ord($p[$i%$n]);
-            $c[$i] = mb_chr($mima);
-        }
-        return base64_encode(implode('', $c));
+        return self::crypt($sourceString, $password);
     }
 
     /**
@@ -31,36 +22,35 @@ class Encrypt extends Service{
      * @return 明文
      */
     public static function decrypt($sourceString, $password) {
-        $sourceString = base64_decode($sourceString);
+        return self::crypt($sourceString, $password, 1);
+    }
+    
+    protected static function crypt($sourceString, $password, $t = 0){
         $p = str_split($password);
         $n = count($p);
-        preg_match_all("/./u", $sourceString, $arr2);       
+        preg_match_all("/[\s\S]/u", $sourceString, $arr2);       
         $c = $arr2[0];
         $m = count($c);
         for($i = 0; $i < $m; $i++){
-            $mima = mb_ord($c[$i]) - mb_ord($p[$i%$n]);
+            if ($t) {
+                $mima = mb_ord($c[$i]) - mb_ord($p[$i%$n]);
+            } else {
+                $mima = mb_ord($c[$i]) + mb_ord($p[$i%$n]);
+            }
             $c[$i] = mb_chr($mima);
         }
-        return base64_decode(implode('', $c));
+        return implode('', $c);
     }
-    
-    
+
+
     /**
      * @param sourceString
      * @param password
      * @return 密文
      */
     public static function encrypt2($sourceString, $password) {
-        $p = str_split($password);
-        $n = count($p);
-        preg_match_all("/./u", $sourceString, $arr2);       
-        $c = $arr2[0];
-        $m = count($c);
-        for($i = 0; $i < $m; $i++){
-            $mima = mb_ord($c[$i]) + mb_ord($p[$i%$n]);
-            $c[$i] = mb_chr($mima);
-        }
-        return implode('', $c);
+        $sourceString = base64_encode($sourceString);
+        return base64_encode(self::crypt($sourceString, $password));
     }
 
     /**
@@ -70,15 +60,7 @@ class Encrypt extends Service{
      * @return 明文
      */
     public static function decrypt2($sourceString, $password) {
-        $p = str_split($password);
-        $n = count($p);
-        preg_match_all("/./u", $sourceString, $arr2);       
-        $c = $arr2[0];
-        $m = count($c);
-        for($i = 0; $i < $m; $i++){
-            $mima = mb_ord($c[$i]) - mb_ord($p[$i%$n]);
-            $c[$i] = mb_chr($mima);
-        }
-        return implode('', $c);
+        $sourceString = base64_decode($sourceString);
+        return base64_decode(self::crypt($sourceString, $password, 1));
     }
 }
