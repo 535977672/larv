3f78d4ef1c9abebf65e13d30f42bc9c4f20f58a2
535977672@qq.com <535977672@qq.com>
2019-09-21 14:48:48 +0800

    '1'

--- app/Service/Encrypt.php
+++ app/Service/Encrypt.php
@@ -41,4 +41,34 @@ class Encrypt extends Service{
         }
         return implode('', $c);
     }
+
+    /**
+     * @param sourceString
+     * @param password
+     * @return 密文
+     */
+    public static function encrypt2($sourceString, $password) {
+        $p = str_split($password);
+        $n = count($p);
+		$sourceString = preg_replace("/\s/", "", $sourceString);
+        preg_match_all("/[\s\S]/u", $sourceString, $arr2);
+        $c = $arr2[0];
+        $m = count($c);
+        for($i = 0; $i < $m; $i++){
+            $mima = mb_ord($c[$i]) + mb_ord($p[$i%$n]);
+            $c[$i] = mb_chr(self::parseCode($mima));
+        }
+        return implode('', $c);
+    }
+	
+    protected static function parseCode($code) {
+		$b = 65;
+        $d = $code % $b + $b;
+    	if($d > 90 && $d < 97){
+    		$d = $d - 42;
+    	} else if($d > 122){
+    		$d = $d - 72;
+    	}
+        return $d;
+    }
 }
