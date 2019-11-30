34dec89eec1ac5e0049c8544d4a4a72dc9c05158
535977672@qq.com <535977672@qq.com>
2019-11-30 17:31:59 +0800

    1

--- public/js/main.js
+++ public/js/main.js
@@ -585,15 +585,15 @@ function initPay(){
                 minute = Math.floor(intDiff / 60) - (day * 24 * 60) - (hour * 60);
                 second = Math.floor(intDiff) - (day * 24 * 60 * 60) - (hour * 60 * 60) - (minute * 60);
             }
+            if (hour <=0 && minute <= 0 && second <= 0) {
+                qrcode_timeout();
+                clearInterval(myTimer);
+            }
             if (minute <= 9) minute = '0' + minute;
             if (second <= 9) second = '0' + second;
             $('#hour_show').html('<s id="h"></s>' + hour + '时');
             $('#minute_show').html('<s></s>' + minute + '分');
             $('#second_show').html('<s></s>' + second + '秒');
-            if (minute <= 0 && second <= 0) {
-                qrcode_timeout();
-                clearInterval(myTimer);
-            }
             intDiff--;
             checkdata();
         }, 1000);
